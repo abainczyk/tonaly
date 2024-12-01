535cc0dd977e570b7aae85dc5482471e52985873
Alexander Bainczyk <alexander.bainczyk@iits-consulting.de>
2024-12-01 20:42:28 +0100

    add base path to router

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./components/Home.tsx";
+import { BASE_URL } from "./config/environments.ts";
 
 export const router = createBrowserRouter([
 	{
 		path: "",
 		element: <Home />,
 	},
-]);
+], {
+	basename: BASE_URL
+});
